b422b21da697ed486f1a339f581135a95ee6e4fb
Andrew Shaban <andreyshaban@gmail.com>
2019-06-09 13:45:23 +0300

    Updates icons.

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,6 +1,7 @@
 <?php
 
 use yii\helpers\Html;
+use yii\helpers\Url;
 
 /* @var $this \yii\web\View */
 /* @var $content string */
@@ -17,12 +18,27 @@ use yii\helpers\Html;
     <title><?= Html::encode($this->title) ?></title>
     <link rel="stylesheet" href="<?= Yii::$app->request->getBaseUrl() ?>/css/site.css"/>
     <?php $this->head() ?>
+    <style>
+        .header img {
+            height: 50px;
+            text-align: center;
+        }
+
+        .header div {
+            text-align: center;
+        }
+
+        .header h1 {
+            padding: 0 10px 0 10px;
+        }
+    </style>
 </head>
 <body>
 <?php $this->beginBody() ?>
 <div class="header">
     <h1><?= Html::a('Экозов', ['/']) ?></h1>
-    <a></a><?= Html::a('сообщить о проблеме', ['/']) ?></a>
+    <div><img src="<?= Url::base() . '/images/icons/alarm_for_user.svg' ?>" alt="alarm_for_user">
+        <img src="<?= Url::base() . '/images/icons/megaphone_for_business.svg' ?>" alt="megaphone_for_business"></div>
 </div>
 
 <div class="content">
