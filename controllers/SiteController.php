fe8ba58ee4b3fc6ca253b4bbdaadb0b9cad82d67
Andrew Shaban <andreyshaban@gmail.com>
2019-06-09 04:57:52 +0300

    Adds map.

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -2,11 +2,10 @@
 
 namespace app\controllers;
 
-use Yii;
-use yii\filters\AccessControl;
-use yii\web\Controller;
-use yii\filters\VerbFilter;
 use app\models\LoginForm;
+use yii\filters\AccessControl;
+use yii\filters\VerbFilter;
+use yii\web\Controller;
 
 class SiteController extends Controller
 {
@@ -44,6 +43,89 @@ class SiteController extends Controller
 
     public function actionIndex()
     {
-        return $this->render('index');
+        $items = [
+            [
+                'latitude' => 52.906386,
+                'longitude' => 59.954092,
+                'options' => [
+                    [
+                        'hintContent' => 'Подсказка при наведении на маркет',
+                        'balloonContentHeader' => 'Заголовок после нажатия на маркер',
+                        'balloonContentBody' => 'Контент после нажатия на маркер',
+                        'balloonContentFooter' => 'Футер после нажатия на маркер',
+                    ],
+                    [
+                        'preset' => 'islands#icon',
+                        'iconColor' => '#19a111'
+                    ]
+                ]
+            ],
+            [
+                'latitude' => 55.751812,
+                'longitude' => 37.599292,
+                'options' => [
+                    [
+                        'hintContent' => 'Подсказка при наведении на маркет',
+                        'balloonContentHeader' => 'Заголовок после нажатия на маркер',
+                        'balloonContentBody' => 'Контент после нажатия на маркер',
+                        'balloonContentFooter' => 'Футер после нажатия на маркер',
+                    ],
+                    [
+                        'preset' => 'islands#circleIcon',
+                        'iconColor' => '#19aa8d',
+                        'draggable' => true
+                    ]
+                ]
+            ],
+            [
+                'latitude' => 47.250534,
+                'longitude' => 39.682889,
+                'options' => [
+                    [
+                        'hintContent' => 'Подсказка при наведении на маркет',
+                        'balloonContentHeader' => 'Заголовок после нажатия на маркер',
+                        'balloonContentBody' => 'Контент после нажатия на маркер',
+                        'balloonContentFooter' => 'Футер после нажатия на маркер',
+                    ],
+                    [
+                        'preset' => 'islands#blueCircleDotIconWithCaption',
+                        'iconColor' => '#19aa8d'
+                    ]
+                ]
+            ],
+            [
+                'latitude' => 58.091523,
+                'longitude' => 57.805861,
+                'options' => [
+                    [
+                        'hintContent' => 'Подсказка при наведении на маркет',
+                        'balloonContentHeader' => 'Заголовок после нажатия на маркер',
+                        'balloonContentBody' => 'Контент после нажатия на маркер',
+                        'balloonContentFooter' => 'Футер после нажатия на маркер',
+                    ],
+                    [
+                        'preset' => 'islands#redSportIcon',
+                        'iconColor' => '#19aa8d'
+                    ]
+                ]
+            ],
+            [
+                'latitude' => 60.091523,
+                'longitude' => 75.805861,
+                'options' => [
+                    [
+                        'hintContent' => 'Подсказка при наведении на маркет',
+                        'balloonContentHeader' => 'Заголовок после нажатия на маркер',
+                        'balloonContentBody' => 'Контент после нажатия на маркер',
+                        'balloonContentFooter' => 'Футер после нажатия на маркер',
+                    ],
+                    [
+                        'preset' => 'islands#governmentCircleIcon',
+                        'iconColor' => '#3b5998'
+                    ]
+                ]
+            ],
+        ];
+        return $this->render('index', ['items' => $items]);
     }
 }
--- views/site/index.php
+++ views/site/index.php
@@ -1,7 +1,23 @@
 <?php
 /* @var $this yii\web\View */
 $this->title = 'My Yii Application';
-?>
-<div class="site-index">
-  Start
-</div>
+
+use phpnt\yandexMap\YandexMaps; ?>
+
+<?= YandexMaps::widget([
+    'myPlacemarks'          => $items,
+    'mapOptions'            => [
+        'center'            => [52, 59],                                                // центр карты
+        'zoom'              => 3,                                                       // показывать в масштабе
+        'controls'          => ['zoomControl',  'fullscreenControl'],  // использовать эл. управления
+        'control'           => [
+            'zoomControl'   => [                                                        // расположение кнопок управлением масштабом
+                'top'       => 75,
+                'left'      => 5
+            ],
+        ],
+    ],
+    'disableScroll'         => true,                                                    // отключить скролл колесиком мыши (по умолчанию true)
+    'windowWidth'           => '100%',                                                  // длинна карты (по умолчанию 100%)
+    'windowHeight'          => '400px',                                                 // высота карты (по умолчанию 400px)
+]);?>
